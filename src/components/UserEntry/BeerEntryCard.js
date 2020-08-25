cd0522825f0e0337524b32745474665198b19232
TimDunphy <timothy.dunphy@gmail.com>
2020-08-25 16:42:16 +0100

    Extracted BeerEntryCard into it's own component.

---
+++ src/components/UserEntry/BeerEntryCard.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+import { containers } from '../../helpers/containers'
+import { ABVInput } from './ABVInput'
+import { RadioButton } from './RadioButton'
+
+const BeerEntryCard = ({ drinks, setDrinks, currentDrink, objectRef }) => {
+    const beerEntryVariant = {
+        hidden: { opacity: 0.2, x: currentDrink === 1 ? '-30px' : '30px' },
+        animate: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 0.25,
+                ease: [0, 0, 0.2, 1]
+            }
+        },
+        exit: {
+            opacity: 0.2,
+            x: currentDrink === 1 ? '-30px' : '30px',
+            transition: {
+                duration: 0.2,
+                ease: [0.4, 0, 1, 1]
+            }
+        }
+    }
+
+    return (
+        <motion.div
+            className="relative flex flex-col items-center justify-center flex-grow w-4/5 bg-white rounded-md p-2 shadow-xs"
+            variants={beerEntryVariant}
+            initial="hidden"
+            animate="animate"
+            exit="exit"
+        >
+            <p className="absolute bottom-0 right-0 p-2 text-sm font-semibold">
+                Beer #{currentDrink}
+            </p>
+            <p className="pb-2 text-center text-lg underline">Percentage</p>
+            <ABVInput
+                key={`beer-abv-${currentDrink}`}
+                placeholder="5.6"
+                value={drinks[objectRef].abv}
+                moveAway={e => {
+                    const newAbv = Number(e.currentTarget.value)
+                    setDrinks(drinks => ({
+                        ...drinks,
+                        [objectRef]: {
+                            ...drinks[objectRef],
+                            abv: newAbv
+                        }
+                    }))
+                }}
+            />
+            <p className="py-2 text-center text-lg underline">Volume</p>
+            <div className="flex flex-wrap mb-2">
+                {Object.values(containers).map(container => {
+                    return (
+                        <RadioButton
+                            stringIndex="one"
+                            container={container}
+                            click={() => {
+                                setDrinks(drinks => ({
+                                    ...drinks,
+                                    [objectRef]: {
+                                        ...drinks[objectRef],
+                                        volume: container.volume
+                                    }
+                                }))
+                            }}
+                            selected={drinks[objectRef].volume}
+                            key={`${container.volume}-${currentDrink}`}
+                        />
+                    )
+                })}
+            </div>
+        </motion.div>
+    )
+}
+
+export { BeerEntryCard }
--- src/screens/UserEntry.js
+++ src/screens/UserEntry.js
@@ -3,32 +3,10 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { Link } from 'react-router-dom'
 
 import { containerVariant } from '../motion/variants'
-import { containers } from '../helpers/containers'
-import { ABVInput } from '../components/UserEntry/ABVInput'
-import { RadioButton } from '../components/UserEntry/RadioButton'
+import { BeerEntryCard } from '../components/UserEntry/BeerEntryCard'
 
 const UserEntry = ({ drinks, setDrinks }) => {
-    const [currentBeer, setCurrentBeer] = useState(1)
-
-    const beerEntryVariant = {
-        hidden: { opacity: 0.2, x: currentBeer === 1 ? '-30px' : '30px' },
-        animate: {
-            opacity: 1,
-            x: 0,
-            transition: {
-                duration: 0.25,
-                ease: [0, 0, 0.2, 1]
-            }
-        },
-        exit: {
-            opacity: 0.2,
-            x: currentBeer === 1 ? '-30px' : '30px',
-            transition: {
-                duration: 0.2,
-                ease: [0.4, 0, 1, 1]
-            }
-        }
-    }
+    const [currentDrink, setCurrentDrink] = useState(1)
 
     return (
         <motion.div
@@ -45,128 +23,40 @@ const UserEntry = ({ drinks, setDrinks }) => {
             </h2>
             <hr className="border-primary-dark opacity-disabled mt-2 mb-4 w-full" />
             <AnimatePresence initial={false} exitBeforeEnter>
-                {currentBeer === 1 ? (
-                    <motion.div
-                        key="beer-entry-one"
-                        className="flex flex-col items-center justify-center flex-grow w-4/5 bg-white rounded-md p-2 shadow-xs"
-                        variants={beerEntryVariant}
-                        initial="hidden"
-                        animate="animate"
-                        exit="exit"
-                    >
-                        <p className="pb-2 text-center text-lg">
-                            What percentage is the first beer to compare?
-                        </p>
-                        <ABVInput
-                            key="beer-one"
-                            placeholder="6.2"
-                            value={drinks.drinkOne.abv}
-                            moveAway={e => {
-                                const newAbv = Number(e.currentTarget.value)
-                                setDrinks(drinks => ({
-                                    ...drinks,
-                                    drinkOne: {
-                                        ...drinks.drinkOne,
-                                        abv: newAbv
-                                    }
-                                }))
-                            }}
-                        />
-                        <p className="py-2 text-center text-lg">
-                            What size is the drink?
-                        </p>
-                        <div className="flex flex-wrap mb-2">
-                            {Object.values(containers).map(container => {
-                                return (
-                                    <RadioButton
-                                        stringIndex="one"
-                                        container={container}
-                                        click={() => {
-                                            setDrinks(drinks => ({
-                                                ...drinks,
-                                                drinkOne: {
-                                                    ...drinks.drinkOne,
-                                                    volume: container.volume
-                                                }
-                                            }))
-                                        }}
-                                        selected={drinks.drinkOne.volume}
-                                        key={`${container.volume}-one`}
-                                    />
-                                )
-                            })}
-                        </div>
-                    </motion.div>
+                {currentDrink === 1 ? (
+                    <BeerEntryCard
+                        key={`BeerEntryCard${currentDrink}`}
+                        drinks={drinks}
+                        setDrinks={setDrinks}
+                        currentDrink={currentDrink}
+                        objectRef="drinkOne"
+                    />
                 ) : (
-                    <motion.div
-                        key="beer-entry-two"
-                        className="flex flex-col items-center justify-center flex-grow w-4/5 bg-white rounded-md p-2 shadow-xs"
-                        variants={beerEntryVariant}
-                        initial="hidden"
-                        animate="animate"
-                        exit="exit"
-                    >
-                        <p className="pb-2 text-center text-lg">
-                            What percentage is the second beer to compare?
-                        </p>
-                        <ABVInput
-                            key="beer-two"
-                            placeholder="5.6"
-                            value={drinks.drinkTwo.abv}
-                            moveAway={e => {
-                                const newAbv = Number(e.currentTarget.value)
-                                setDrinks(drinks => ({
-                                    ...drinks,
-                                    drinkTwo: {
-                                        ...drinks.drinkTwo,
-                                        abv: newAbv
-                                    }
-                                }))
-                            }}
-                        />
-                        <p className="py-2 text-center text-lg">
-                            What size is the drink?
-                        </p>
-                        <div className="flex flex-wrap mb-2">
-                            {Object.values(containers).map(container => {
-                                return (
-                                    <RadioButton
-                                        stringIndex="two"
-                                        container={container}
-                                        click={() => {
-                                            setDrinks(drinks => ({
-                                                ...drinks,
-                                                drinkTwo: {
-                                                    ...drinks.drinkTwo,
-                                                    volume: container.volume
-                                                }
-                                            }))
-                                        }}
-                                        selected={drinks.drinkTwo.volume}
-                                        key={`${container.volume}-two`}
-                                    />
-                                )
-                            })}
-                        </div>
-                    </motion.div>
+                    <BeerEntryCard
+                        key={`BeerEntryCard${currentDrink}`}
+                        drinks={drinks}
+                        setDrinks={setDrinks}
+                        currentDrink={currentDrink}
+                        objectRef="drinkTwo"
+                    />
                 )}
             </AnimatePresence>
             <div className="flex justify-evenly w-4/5 mt-4 mb-8 flex-none">
                 <button
                     className={
-                        currentBeer === 1 ? 'disabled-btn' : 'active-btn'
+                        currentDrink === 1 ? 'disabled-btn' : 'active-btn'
                     }
                     onClick={() => {
-                        setCurrentBeer(1)
+                        setCurrentDrink(1)
                     }}
                 >
                     Prev Beer
                 </button>
-                {currentBeer === 1 ? (
+                {currentDrink === 1 ? (
                     <button
                         className="active-btn"
                         onClick={() => {
-                            setCurrentBeer(2)
+                            setCurrentDrink(2)
                         }}
                     >
                         Next Beer
