3ff025288e8c3bcb424461a6d4fe7285ba9d5b75
TimDunphy <timothy.dunphy@gmail.com>
2020-08-25 15:37:43 +0100

    Fixed z index issue with header.

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ const App = () => {
 
     return (
         <div className="overflow-hidden">
-            <div className="w-full flex fixed top-0">
+            <div className="w-full flex fixed top-0 z-20">
                 <ThresholdSettings
                     threshold={threshold}
                     setThreshold={setThreshold}
